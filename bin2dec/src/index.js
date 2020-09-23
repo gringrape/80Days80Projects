82061e796900bb00058f4bb0ba025bba333208c2
gringrape <53764714+gringrape@users.noreply.github.com>
2020-09-23 19:45:49 +0900

    Render 'Hello React'

---
+++ bin2dec/src/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  'hello, react!',
+  document.getElementById('app'),
+);
---
+++ bin2dec/webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    compress: true,
+    port: 9000,
+    open: true,
+    hot: true,
+    watchContentBase: true,
+  },
+};
